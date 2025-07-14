c761f73e64f39b326cff18d6658264192125bfac
Christian Deacon <christianmdeacon@gmail.com>
2025-07-14 18:57:04 -0400

    Implement multi-threading using pthreads into main server.

--- src/cweb/server/server.h
+++ src/cweb/server/server.h
@@ -7,9 +7,14 @@
 #include <cweb/http/request.h>
 #include <cweb/http/response.h>
 #include <cweb/fs/web.h>
+#include <cweb/server/thread.h>
 
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <unistd.h>
-int server__setup(ctx_t* ctx);
+
+#include <sys/sysinfo.h>
+
+int server__setup(ctx_t* ctx, int threads);
+int server__shutdown(int threads);
