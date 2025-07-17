826918edf54471248d21536ee464e3bdbd5d3d80
Christian Deacon <christianmdeacon@gmail.com>
2025-07-17 01:48:08 -0400

    Add ERR_SET() macro and functions in header.

--- src/utils/error/error.h
+++ src/utils/error/error.h
@@ -11,8 +11,13 @@
 
 #define ERR_MSG_MAX_LEN 1024
 
+#define ERR_SET(code, msg, ...) utils__error_set((code), __func__, (msg), ##__VA_ARGS__)
+
 struct error_ctx {
     int code;
     char msg[ERR_MSG_MAX_LEN];
     char func[MAX_NAME_LEN];
 } typedef error_ctx_t;
+
+void utils__error_set(int code, const char* func, const char* msg, ...);
+error_ctx_t* utils__error_ctx();
